09db645651964891ce346538dfae247edec9b937
Adam Karpierz <akarpierz@gmail.com>
2017-08-23 08:04:30 +0200

    ...

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+libusb documentation
+====================
+
+.. include:: README.rst
+
+Parts of the documentation
+==========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   CHANGES.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ tests/stress.py
@@ -0,0 +1,158 @@
+/*
+ * libusb stress test program to perform simple stress tests
+ * Copyright © 2012 Toby Gray <toby.gray@realvnc.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libusb.h"
+#include "libusb_testlib.h"
+
+
+static libusb_testlib_result test_init_and_exit(libusb_testlib_ctx * tctx)
+{
+    /** Test that creates and destroys a single concurrent context
+     * 10000 times. */
+
+    libusb_context * ctx = NULL;
+    for i in range(10000):
+        int r = libusb.init(&ctx)
+        if r != libusb.LIBUSB_SUCCESS:
+            libusb_testlib_logf(tctx, "Failed to init libusb on iteration %d: %d", i, r);
+            return TEST_STATUS_FAILURE;
+        }
+        libusb.exit(ctx)
+        ctx = NULL;
+
+    return TEST_STATUS_SUCCESS;
+}
+
+static libusb_testlib_result test_get_device_list(libusb_testlib_ctx * tctx)
+{
+    /** Tests that devices can be listed 1000 times. */
+
+    libusb_context * ctx = NULL;
+    int r, i;
+    r = libusb.init(&ctx)
+    if r != libusb.LIBUSB_SUCCESS:
+        libusb_testlib_logf(tctx, "Failed to init libusb: %d", r);
+        return TEST_STATUS_FAILURE;
+
+    for (i = 0; i < 1000; ++i) {
+        libusb_device ** device_list;
+        ssize_t list_size = libusb.get_device_list(ctx, &device_list);
+        if (list_size < 0 || device_list == NULL) {
+            libusb_testlib_logf(tctx,
+                "Failed to get device list on iteration %d: %d (%p)",
+                i, -list_size, device_list);
+            return TEST_STATUS_FAILURE;
+        }
+        libusb.free_device_list(device_list, 1);
+    }
+    libusb.exit(ctx)
+    return TEST_STATUS_SUCCESS;
+}
+
+static libusb_testlib_result test_many_device_lists(libusb_testlib_ctx * tctx)
+{
+    /** Tests that 100 concurrent device lists can be open at a time. */
+
+#define LIST_COUNT 100
+
+    libusb_context * ctx = NULL;
+    libusb_device ** device_lists[LIST_COUNT];
+    int r, i;
+    memset(device_lists, 0, sizeof(device_lists));
+
+    r = libusb.init(&ctx)
+    if r != libusb.LIBUSB_SUCCESS:
+        libusb_testlib_logf(tctx, "Failed to init libusb: %d", r);
+        return TEST_STATUS_FAILURE;
+    }
+
+    /* Create the 100 device lists. */
+    for i in range(LIST_COUNT):
+        ssize_t list_size = libusb.get_device_list(ctx, &(device_lists[i]));
+        if (list_size < 0 || device_lists[i] == NULL) {
+            libusb_testlib_logf(tctx,
+                "Failed to get device list on iteration %d: %d (%p)",
+                i, -list_size, device_lists[i]);
+            return TEST_STATUS_FAILURE;
+        }
+    }
+
+    /* Destroy the 100 device lists. */
+    for i in range(LIST_COUNT):
+        if (device_lists[i]) {
+            libusb.free_device_list(device_lists[i], 1);
+            device_lists[i] = NULL;
+        }
+    }
+
+    libusb.exit(ctx)
+    return TEST_STATUS_SUCCESS;
+
+#undef LIST_COUNT
+}
+
+static libusb_testlib_result test_default_context_change(libusb_testlib_ctx * tctx)
+{
+    /** Tests that the default context (used for various things including
+     * logging) works correctly when the first context created in a
+     * process is destroyed. */
+
+    libusb_context * ctx = NULL;
+    int r, i;
+
+    for i in range(100):
+
+        /* First create a new context */
+        r = libusb.init(&ctx)
+        if r != libusb.LIBUSB_SUCCESS:
+            libusb_testlib_logf(tctx, "Failed to init libusb: %d", r);
+            return TEST_STATUS_FAILURE;
+        }
+
+        /* Enable debug output, to be sure to use the context */
+        libusb.set_debug(NULL, LIBUSB_LOG_LEVEL_DEBUG)
+        libusb.set_debug(ctx,  LIBUSB_LOG_LEVEL_DEBUG)
+
+        /* Now create a reference to the default context */
+        r = libusb.init(None)
+        if r != libusb.LIBUSB_SUCCESS:
+            libusb_testlib_logf(tctx, "Failed to init libusb: %d", r);
+            return TEST_STATUS_FAILURE;
+        }
+
+        /* Destroy the first context */
+        libusb.exit(ctx)
+        /* Destroy the default context */
+        libusb.exit(None)
+
+    return TEST_STATUS_SUCCESS;
+}
+
+/* Fill in the list of tests. */
+static const libusb_testlib_test tests[] = {
+    {"init_and_exit",          &test_init_and_exit},
+    {"get_device_list",        &test_get_device_list},
+    {"many_device_lists",      &test_many_device_lists},
+    {"default_context_change", &test_default_context_change},
+    LIBUSB_NULL_TEST
+};
+
+def main():
+
+    return libusb_testlib_run_tests(sys.argv, tests)
