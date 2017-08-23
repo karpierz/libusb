b63ce35ce408f83fa775773b874b5a8ca9c65333
Adam Karpierz <akarpierz@gmail.com>
2017-08-23 08:42:58 +0200

    ...

--- libusb/__about__.py
+++ libusb/__about__.py
@@ -7,7 +7,7 @@ __all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
 __title__        = "libusb"
 __summary__      = "Python wrapper for the libusb library (ctypes/cffi-based libusb)"
 __uri__          = "http://pypi.python.org/pypi/libusb/"
-__version_info__ = type("version_info", (), dict(serial=1,
+__version_info__ = type("version_info", (), dict(serial=2,
                         major=1, minor=0, micro=21, releaselevel="alpha"))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(final="", alpha="a", beta="b", rc="rc")[__version_info__.releaselevel],
