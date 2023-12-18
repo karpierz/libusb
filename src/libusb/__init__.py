a6029b4e851e3c24c1ae5c4bcdbf06736bef728d
Adam Karpierz <adam.karpierz@aptiv.com>
2023-12-18 09:00:13 +0100

    1.0.26rc1 (2023-12-15) ----------------------

--- src/libusb/__init__.py
+++ src/libusb/__init__.py
@@ -6,4 +6,4 @@ from .__about__ import * ; del __about__  # noqa
 from . import __config__ ; del __config__
 from .__config__ import set_config as config
 
-from ._libusb import * ; del _libusb # noqa
+from ._libusb import * ; del _libusb  # noqa
